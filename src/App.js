a689cd3df1e33d651a88dd96f02de76b17207147
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-01 14:53:34 +0900

    smart contract

--- src/App.js
+++ src/App.js
@@ -4,5 +4,16 @@ import Web3 from "web3";
 export default function App() {
   const [account, setAccount] = useState();
 
-  return <div></div>;
+  useEffect(() => {
+    async function load() {
+      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+      const accounts = await web3.eth.requestAccounts();
+
+      setAccount(accounts[0]);
+    }
+
+    load();
+  }, []);
+
+  return <div>Your account is: {account}</div>;
 }
