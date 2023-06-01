59a643e7623d9395b4d86089011b95d963ee48ee
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-06-01 16:07:59 +0900

    ABi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
+import { CONTACT_ABI, CONTACT_ADDRESS } from "./config";
 
 export default function App() {
   const [account, setAccount] = useState();
+  const [contactList, setContactList] = useState();
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     async function load() {
       const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
       const accounts = await web3.eth.requestAccounts();
-
       setAccount(accounts[0]);
+      const contactList = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+      setContactList(contactList);
+      const counter = await contactList.methods.count().call();
+      for (var i = 1; i <= counter; i++) {
+        const contact = await contactList.methods.contacts(i).call();
+        setContacts((contacts) => [...contacts, contact]);
+      }
     }
 
     load();
